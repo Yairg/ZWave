20b36f062b70998c0b122ca2f3d7d3bfc913814c
unknown <yairg@YAIRG-LAPT.redmond.corp.microsoft.com>
2012-09-24 00:05:43 -0700

    Fix some bugs

--- zwave/Program.cs
+++ zwave/Program.cs
@@ -31,7 +31,7 @@ namespace ZWave
             public byte Level { get; set; }
         }
         [DataContract]
-        public class Schene
+        public class Scene
         {
             [DataMember]
             public string Name { get; set; }
@@ -62,12 +62,12 @@ namespace ZWave
             public IEnumerable<Device> Devices { get; set; }
 
             [DataMember]
-            public IEnumerable<Schene> Schenes { get; set; }
+            public IEnumerable<Scene> Scenes { get; set; }
 
             [DataMember]
             public IEnumerable<Controller> Controllers { get; set; }
 
-            internal Schene GetScheneById(string id)
+            internal Scene GetSceneById(string id)
             {
                 if (id.StartsWith("d:"))
                 {
@@ -78,18 +78,18 @@ namespace ZWave
                     }
                     var deviceId = Int32.Parse(idvalues[1]);
                     var devicesArray = new int[] { deviceId };
-                    var resultSchene = new Schene { Id = id, Name = id };
+                    var resultScene = new Scene { Id = id, Name = id };
                     if (String.Compare(idvalues[2], "on", ignoreCase: true) == 0)
                     {
-                        resultSchene.OnDevices = devicesArray;
+                        resultScene.OnDevices = devicesArray;
                     }
                     else
                     {
-                        resultSchene.OffDevices = devicesArray;
+                        resultScene.OffDevices = devicesArray;
                     }
-                    return resultSchene;
+                    return resultScene;
                 }
-                return this.Schenes.Where(Schene => Schene.Id == id).First();
+                return this.Scenes.Where(Scene => Scene.Id == id).First();
             }
 
             internal Device GetDeviceById(int id)
@@ -104,17 +104,17 @@ namespace ZWave
         }
 
         [DataContract]
-        public class ScheneToControllerMap
+        public class SceneToControllerMap
         {
             [DataMember]
-            public string ScheneId { get; set; }
+            public string SceneId { get; set; }
 
             [DataMember]
             public byte Channell { get; set; }
 
-            public ScheneToControllerMap()
+            public SceneToControllerMap()
             {
-                ScheneId = null;
+                SceneId = null;
                 Channell = 0;
             }
         }
@@ -130,7 +130,7 @@ namespace ZWave
             public int NumberOfChannels { get; set; }
 
             [IgnoreDataMember]
-            public IEnumerable<ScheneToControllerMap> Schenes { get; set; }
+            public IEnumerable<SceneToControllerMap> Scenes { get; set; }
 
             [DataMember]
             public string[] Channells
@@ -146,90 +146,23 @@ namespace ZWave
                 }
             }
 
-            public void SetMapping(string[] Schenes)
+            public void SetMapping(string[] Scenes)
             {
-                List<ScheneToControllerMap> mappings = new List<ScheneToControllerMap>();
-                for (byte i = 0; i < NumberOfChannels && i < Schenes.Length; i++)
+                List<SceneToControllerMap> mappings = new List<SceneToControllerMap>();
+                for (byte i = 0; i < NumberOfChannels && i < Scenes.Length; i++)
                 {
-                    if (!String.IsNullOrEmpty(Schenes[i]))
+                    if (!String.IsNullOrEmpty(Scenes[i]))
                     {
-                        mappings.Add(new ScheneToControllerMap { Channell = (byte)(i + 1), ScheneId = Schenes[i] });
+                        mappings.Add(new SceneToControllerMap { Channell = (byte)(i + 1), SceneId = Scenes[i] });
                     }
                 }
-                this.Schenes = mappings;
+                this.Scenes = mappings;
             }
         }
     };
 
     class Program
     {
-        static ConfigurationFile.ConfigFile SaveConfig()
-        {
-            var devices = new ConfigurationFile.Device[] {
-                
-                new ConfigurationFile.Device { Name = "מסדרון למעלה ליד החדרים", NodeId = 1},
-                new ConfigurationFile.Device { Name = "מסדרון למטה", NodeId = 4}, 
-                new ConfigurationFile.Device { NodeId = 6 , Name ="שקע בסלון"},
-                new ConfigurationFile.Device { NodeId =7  , Name ="חדר ארונות הורים"},
-                new ConfigurationFile.Device { NodeId =8 , Name ="מיטה יאיר"},
-                new ConfigurationFile.Device { NodeId =9 , Name ="מיטה מוריה"},
-                new ConfigurationFile.Device { NodeId =11 , Name ="סלון"},
-                new ConfigurationFile.Device { NodeId =12 , Name ="שקע בסלום"},
-                new ConfigurationFile.Device { NodeId =14 , Name ="חדר ילדים"},
-                new ConfigurationFile.Device { NodeId =15 , Name ="חדר משחקים"},
-                new ConfigurationFile.Device { NodeId =16 , Name ="מסדרון למעלה קרוב למטבח"},
-                new ConfigurationFile.Device { NodeId =17 , Name ="גראז ליד הדלת"},
-                new ConfigurationFile.Device { NodeId =18 , Name ="גראז מעל המדפים"},
-                new ConfigurationFile.Device { NodeId =19 , Name ="גראז מעל הסולם"},
-                new ConfigurationFile.Device { NodeId =20 , Name ="שירותים למטה מעל הכיור"},
-                new ConfigurationFile.Device { NodeId =21 , Name ="למטה מעל מכונת הכביסה"},
-                new ConfigurationFile.Device { NodeId =22 , Name ="למטה מעל הטלוויזיה"},
-                new ConfigurationFile.Device { NodeId =23 , Name ="למטה מעל המחשב"},
-                new ConfigurationFile.Device { NodeId =25 , Name ="למטה מעל המיטה"},
-                new ConfigurationFile.Device { NodeId =26 , Name ="מטבח מעל הכיור"},
-                new ConfigurationFile.Device { NodeId =27 , Name ="מטבח מעל המקרר"},
-                new ConfigurationFile.Device { NodeId =28 , Name ="מטבח פינה למעלה (מעל המזווה)"},
-            };
-            var schems = new ConfigurationFile.Schene[] {
-                new ConfigurationFile.Schene { Id = "1", Name = "Downstairs", OnDevices = new int[] { 23, 25, 22 } },
-                new ConfigurationFile.Schene { Id = "C1", Name = "Childrens Room", OnDevices = new int[] { 14 }},
-                new ConfigurationFile.Schene { Id = "C2", Name = "Toy Room", OnDevices = new int[] { 15 }},
-                new ConfigurationFile.Schene { Id = "C3", Name = "Upstairs Corridor", OnDevices = new int[] { 1, 16 }},
-                new ConfigurationFile.Schene { Id = "C4", Name = "Upstairs Corridor Night Schene", OnDevices = new int[] { 16 }, OffDevices = new int[] {1}},
-                new ConfigurationFile.Schene { Id = "P2", Name = "Yair Bed", OnDevices = new int[] { 8 }},
-                new ConfigurationFile.Schene { Id = "P3", Name = "Moriya Bed", OnDevices = new int[] { 9 }},
-                new ConfigurationFile.Schene { Id = "P5", Name = "Closet Room", OnDevices = new int[] { 7 }},
-                new ConfigurationFile.Schene { Id = "PR1", Name= "Welcome Home", OnDevices = new int[] {1,4,16, 27}},
-                new ConfigurationFile.Schene { Id = "PR2", Name= "Night Off", OnDevices = new int[] {16},  OffDevices = new int[] {1, 4, 6, 7, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28}},
-                new ConfigurationFile.Schene { Id = "E1", Name = "Living Room Light", OnDevices = new int[] {6}},
-                new ConfigurationFile.Schene { Id = "7", Name = "Kitchen All", OnDevices = new int[] { 26, 27, 28 }},
-                new ConfigurationFile.Schene { Id = "8", Name = "Parents Room all", OnDevices = new int[] {7, 8, 9}},
-                new ConfigurationFile.Schene { Id = "9", Name = "Childrens Area all", OnDevices = new int[] {1, 16, 14, 15 }},
-                new ConfigurationFile.Schene { Id = "10", Name = "Living Room", OnDevices = new int[] { 6, 11, 12 }},
-                new ConfigurationFile.Schene { Id = "11", Name = "Downstairs All", OnDevices = new int[] { 20, 21, 22, 23, 25, 4 }},
-                new ConfigurationFile.Schene { Id = "12", Name = "Garage", OnDevices = new int[] { 17, 18, 19 }},
-            };
-            var conrollers = new ConfigurationFile.Controller[] {
-                new ConfigurationFile.Controller { Name = "Entrance", NumberOfChannels=12, Channells = new String[] {"PR1", "PR2", "", "", "E1", "", "7", "8", "9", "10", "11", "12"}},
-                new ConfigurationFile.Controller { Name = "Childrens Room", NumberOfChannels=12, Channells = new String[] {"C1", "C2", "C3", "C4", "8", "7", "7", "8", "9", "10", "11", "12"}},
-                new ConfigurationFile.Controller { Name = "Parents Room", NumberOfChannels=12, Channells = new String[] {"8", "P2", "P3", "C4", "P5", "", "7", "8", "9", "10", "11", "12"}},
-                new ConfigurationFile.Controller { Name = "Portable Remote", NumberOfChannels=6, Channells = new String[] {"8", "P2", "P3", "PR2", "PR1"}}
-            };
-            var config = new ConfigurationFile.ConfigFile();
-            config.Devices = devices;
-            config.Schenes = schems;
-            config.Controllers = conrollers;
-            var ser = new DataContractJsonSerializer(typeof(ConfigurationFile.ConfigFile));
-
-            using (FileStream stream = new FileStream(@"c:\temp\homeconfig.json", FileMode.Create))
-            {
-
-                var writter = JsonReaderWriterFactory.CreateJsonWriter(stream, Encoding.UTF8);
-                ser.WriteObject(writter, config);
-                writter.Flush();
-            }
-            return config;
-        }
         static void Main(string[] args)
         {
             try
@@ -259,24 +192,25 @@ namespace ZWave
                 }
 
                 ZWaveController cont = new ZWaveController();
+                Console.WriteLine("Connecting to Think Stick");
                 cont.Connect();
-
+                Console.WriteLine("Connected");
                 var controllerConfig = config.GetControllerByName(args[1]);
                 Console.WriteLine("Copying network settings and channels to controller " + controllerConfig.Name);
-                var replicationSchenes = new List<ZWaveController.ReplicationScene>();
-                foreach (var s in controllerConfig.Schenes)
+                var replicationScenes = new List<ZWaveController.ReplicationScene>();
+                foreach (var s in controllerConfig.Scenes)
                 {
-                    var repSchene = new ZWaveController.ReplicationScene(s.Channell);
-                    var Schene = config.GetScheneById(s.ScheneId);
-                    Console.WriteLine("Channel " + s.Channell + " --> " + Schene.Name);
-                    foreach (var d in Schene.Devices)
+                    var repScene = new ZWaveController.ReplicationScene(s.Channell);
+                    var Scene = config.GetSceneById(s.SceneId);
+                    Console.WriteLine("Channel " + s.Channell + " --> " + Scene.Name);
+                    foreach (var d in Scene.Devices)
                     {
                         var device = config.GetDeviceById(d.DeviceId);
                         Console.WriteLine("                       " + device.Name + " " + (d.Level == 0 ? "off" : "on"));
                         var deviceFromController = cont.Devices[device.NodeId - 1];
-                        repSchene.SceneItems.Add(deviceFromController, d.Level);
+                        repScene.SceneItems.Add(deviceFromController, d.Level);
                     }
-                    replicationSchenes.Add(repSchene);
+                    replicationScenes.Add(repScene);
                 }
                 Console.WriteLine("Make sure to put the controller in \"Receive Network Configuration\" state ");
                 Console.WriteLine("to do so on HA07");
@@ -287,7 +221,7 @@ namespace ZWave
                 Console.WriteLine();
                 Console.WriteLine("On the HA09 the steps are the same but the lights will flash instead");
                 Console.WriteLine("of the display.");
-                var addedController = cont.AddController(new ZWaveController.ReplicationGroup[] { }, replicationSchenes.ToArray());
+                var addedController = cont.AddController(new ZWaveController.ReplicationGroup[] { }, replicationScenes.ToArray());
                 if (addedController == null)
                 {
                     throw new Exception("Failed to add controller");
